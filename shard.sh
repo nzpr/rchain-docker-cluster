c94a233334dc569cdf952b1c6fb0406d774c8482
nutzipper <1746367+nzpr@users.noreply.github.com>
2024-02-09 08:55:58 +0400

    Make parent folders flag

--- shard.sh
+++ shard.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir data/bootstrap >/dev/null 2>&1
+mkdir -p data/bootstrap >/dev/null 2>&1
 cp resources/bootstrap/node.certificate.pem data/rnode.bootstrap/node.certificate.pem
 cp resources/bootstrap/node.key.pem data/rnode.bootstrap/node.key.pem
 docker-compose  -f ./shard.yml up
